535f0489f1d51bc1a02074eb0dacb829f0a94d22
Maliquem <jael182@gmail.com>
2022-01-11 16:32:55 -0300

    0.2

--- src/main/java/br/com/jael/exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/jael/exerciciossb/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.jael.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ public class ClienteController {
     @GetMapping(path = "/jael")
     public Cliente obterCliente() {
         return new Cliente(28, "Jael Araripe", "018.447.523-64");
+    }
+
+    @GetMapping("/{id}")
+    public Cliente obterClientePorId1(@PathVariable int id) {
+        return new Cliente(id, "Roberto Dias", "248.163.759-46");
     }
 
 }
